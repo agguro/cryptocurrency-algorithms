05bbbb2b15af5a586692c009e6baad59f84a00b9
Agguro <aguasguerreiroroberto@gmail.com>
2022-05-21 16:13:33 +0200

    initial commit

---
+++ MultiMiner/MultiMiner.Engine/Installers/AvailableMiners.cs
@@ -0,0 +1,50 @@
+﻿using MultiMiner.Engine.Data;
+using MultiMiner.Utility.Net;
+using MultiMiner.Utility.OS;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace MultiMiner.Engine.Installers
+{
+    public static class AvailableMiners
+    {
+        public static List<AvailableMiner> GetAvailableMiners(string userAgent)
+        {
+            WebClient webClient = new ApiWebClient();
+            webClient.Headers.Add("user-agent", userAgent);
+
+            string platform = "win32";
+            if (OSVersionPlatform.GetConcretePlatform() == PlatformID.MacOSX)
+                platform = "osx64";
+            
+            //include www. to avoid redirect
+            string url = "http://www.multiminerapp.com/miners?platform=" + platform;
+            string response = webClient.DownloadString(new Uri(url));
+
+            List<AvailableMiner> availableMiners = JsonConvert.DeserializeObject<List<AvailableMiner>>(response);
+
+            FixDownloadUrls(availableMiners);
+
+            return availableMiners;
+        }
+
+        private static void FixDownloadUrls(List<AvailableMiner> availableMiners)
+        {
+            FixBFGMinerDownloadUrl(availableMiners);
+        }
+
+        private static void FixBFGMinerDownloadUrl(List<AvailableMiner> availableMiners)
+        {
+            // BFGMiner files are no longer hosted on luke.dashjr.org - see: http://luke.dashjr.org/programs/bitcoin/files/bfgminer/
+            // Fixing the URL client-side rather than server-side as the server has not been touched in ~6 years
+            var oldMinerUrl = "http://luke.dashjr.org/programs/bitcoin/files/bfgminer/";
+            var newMinerUrl = "http://bfgminer.org/files/";
+            foreach (var miner in availableMiners)
+            {
+                miner.Url = miner.Url.Replace(oldMinerUrl, newMinerUrl);
+            }
+        }
+    }
+}
---
+++ MultiMiner/MultiMiner.WhatToMine/Extensions/CoinInformationExtensions.cs
@@ -0,0 +1,56 @@
+﻿using MultiMiner.CoinApi.Data;
+using MultiMiner.Xgminer.Data;
+using System;
+using System.Linq;
+
+namespace MultiMiner.WhatToMine.Extensions
+{
+    static class CoinInformationExtensions
+    {
+        public static void PopulateFromJson(this CoinInformation coinInformation, Data.ApiCoinInformation apiCoinInformation)
+        {
+            coinInformation.Symbol = FixSymbolName(apiCoinInformation.Tag, apiCoinInformation.Algorithm);
+            coinInformation.Algorithm = FixAlgorithmName(apiCoinInformation.Algorithm);
+            coinInformation.CurrentBlocks = apiCoinInformation.Last_Block;
+            coinInformation.Difficulty = apiCoinInformation.Difficulty;
+            coinInformation.Reward = apiCoinInformation.Block_Reward;
+            coinInformation.NetworkHashRate = apiCoinInformation.Nethash;
+            coinInformation.Profitability = apiCoinInformation.Profitability;
+            coinInformation.AdjustedProfitability = apiCoinInformation.Profitability;
+            coinInformation.AverageProfitability = apiCoinInformation.Profitability24;
+            coinInformation.NetworkHashRate = apiCoinInformation.Nethash;
+
+            if (coinInformation.Symbol.Equals("BTC", StringComparison.OrdinalIgnoreCase))
+                coinInformation.Price = 1;
+            else
+                coinInformation.Price = apiCoinInformation.Exchange_Rate;
+        }
+
+        private static string FixSymbolName(string symbol, string algorithm)
+        {
+            string result = symbol;
+
+            if ("NICEHASH".Equals(result))
+            {
+                result = "NiceHash:" + algorithm.Replace(AlgorithmFullNames.SHA256, AlgorithmNames.SHA256);
+            }
+
+            return result;
+        }
+
+        private static string FixAlgorithmName(string algorithm)
+        {
+            string result = algorithm;
+            if (algorithm.Equals(ApiContext.ScryptNFactor, StringComparison.OrdinalIgnoreCase))
+            {
+                result = AlgorithmFullNames.ScryptN;
+            }
+            else
+            {
+                KnownAlgorithm knownAlgorithm = KnownAlgorithms.Algorithms.SingleOrDefault(a => a.Name.Equals(algorithm, StringComparison.OrdinalIgnoreCase));
+                if (knownAlgorithm != null) result = knownAlgorithm.FullName;
+            }
+            return result;
+        }
+    }
+}
